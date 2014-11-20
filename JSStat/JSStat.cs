08d4edf8d59766af396bed830b208d0a929ed8ac
JeremyStevenson <jeremy@formulatrix.com>
2014-11-20 15:46:32 -0500

    New code posted

---
+++ JSStat/JSStat.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// JSStat by Jeremy Stevenson 11/14/2014
+// jeremynstevenson@gmail.com
+
+namespace JSStat
+{
+  public class Account
+  {
+    public static String LoginURL = "https://mytotalconnectcomfort.com/portal/";
+    public static String LocationsURL = "https://mytotalconnectcomfort.com/portal/Locations";
+    public static String ZonesURL = "https://mytotalconnectcomfort.com/portal/";
+    public static String DeviceControlURL = "https://mytotalconnectcomfort.com/portal/Device/Control/";
+
+    private String username;
+    private String password;
+    private CookieContainer cookies;
+    private DateTime lastLoginTime;
+    private static int timeoutMinutes = 5;
+
+    public List<Location> Locations = new List<Location>();
+
+    public Account( String username, String password )
+    {
+      this.username = username;
+      this.password = password;
+    }
+
+    internal CookieContainer Login()
+    {
+      System.Net.HttpWebRequest request;
+      System.Net.HttpWebResponse response;
+      StreamReader streamReader;
+      String responseString;
+
+      // Check to see if we are already logged in and it hasn't timed out.
+      int cookieAgeMinutes = (int)DateTime.Now.Subtract( lastLoginTime ).TotalMinutes;
+      if( cookies != null && cookieAgeMinutes < timeoutMinutes )
+      {
+//        Tools.Log( "Cookie age is " + cookieAgeMinutes + " minutes, which is less than timeout of " + timeoutMinutes + "minutes. Using existing cookies." );
+        return cookies;
+      }
+
+      // If not already logged in, locate the saved cookies file. 
+      try
+      {
+        cookieAgeMinutes = (int)DateTime.Now.Subtract( File.GetLastWriteTime( "JSStatCookies" ) ).TotalMinutes;
+        if( cookieAgeMinutes < timeoutMinutes )
+        {
+  //        Tools.Log( "Saved cookies file age is " + cookieAgeMinutes + " minutes, which is less than timeout of " + timeoutMinutes + "minutes. Using existing cookies." );
+          FileStream stream = new FileStream( "JSStatCookies", FileMode.Open );
+          BinaryFormatter formatter = new BinaryFormatter();
+          cookies = (CookieContainer)formatter.Deserialize( stream );
+          stream.Close();
+          return cookies;
+        }
+      }
+      catch( Exception ex )
+      {
+        Tools.Log( ex.Message );
+      }
+
+      Tools.Log( "Cookies are expired. Logging in again." );
+
+      cookies = new System.Net.CookieContainer();
+
+      //GET login page and initial cookies
+      request = (HttpWebRequest)System.Net.HttpWebRequest.Create( LoginURL );
+      request.KeepAlive = true;
+      request.CookieContainer = cookies;
+      request.Method = "GET";
+      request.ContentType = "application/x-www-form-urlencoded";
+      request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
+
+      response = (HttpWebResponse)request.GetResponse();
+      streamReader = new StreamReader( response.GetResponseStream() );
+      responseString = streamReader.ReadToEnd();
+
+      cookies.Add( response.Cookies );
+
+      //Login and get authorization cookies
+      request = (HttpWebRequest)System.Net.HttpWebRequest.Create( LoginURL );
+      request.CookieContainer = cookies;
+      request.Method = "POST";
+      request.KeepAlive = true;
+      request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+      request.ContentType = "application/x-www-form-urlencoded";
+      request.Referer = LoginURL;
+      request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
+      string postData = "timeOffset=240&UserName=" + username + "&Password=" + password + "&RememberMe=false";
+      byte[] byteArray = System.Text.Encoding.UTF8.GetBytes( postData );
+      request.ContentLength = byteArray.Length;
+      System.IO.Stream dataStream = request.GetRequestStream();
+      dataStream.Write( byteArray, 0, byteArray.Length );
+      dataStream.Close();
+
+      // Get the response.
+      response = (HttpWebResponse)request.GetResponse();
+      dataStream = response.GetResponseStream();
+      streamReader = new System.IO.StreamReader( dataStream );
+      responseString = streamReader.ReadToEnd();
+      streamReader.Close();
+      dataStream.Close();
+      response.Close();
+
+//      Tools.Log( "Login response string " + responseString );
+
+
+      // check that the login was succesful
+      if( !responseString.Contains( "My Locations" ) )
+        throw new Exception( "Login failed. Check username and password." );
+
+      cookies.Add( response.Cookies );
+
+      // write login cookies to disk
+      try
+      {
+        FileStream stream = new FileStream( "JSStatCookies", FileMode.Create );
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize( stream, cookies );
+        stream.Close();
+      }
+      catch( Exception ex )
+      {
+        Tools.Log( "Error writting cookies to disk. " + ex.ToString() );
+      }
+
+      lastLoginTime = DateTime.Now;
+      return cookies;
+    }
+
+    void GetLocations()
+    {
+      //GET Locations page
+      System.Net.HttpWebRequest request;
+      System.Net.HttpWebResponse response;
+      StreamReader streamReader;
+      String responseString;
+
+      request = (HttpWebRequest)WebRequest.Create( LocationsURL );
+      request.KeepAlive = true;
+      request.CookieContainer = Login();
+      request.Method = "GET";
+      request.ContentType = "application/x-www-form-urlencoded";
+      request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
+
+      response = (HttpWebResponse)request.GetResponse();
+      streamReader = new StreamReader( response.GetResponseStream() );
+      responseString = streamReader.ReadToEnd();
+      response.Close();
+
+      // pull out all "data-id=" values for locations
+      int index = 1;
+      while( true )
+      {
+        String locationID = Tools.Parse( responseString, "data-id=\"", "\" data-url", ref index );
+        if( locationID == null ) break;
+        String locationName = Tools.Parse( responseString, "<div class=\"location-name\">", "</div>", ref index ).Trim();
+        Tools.Log( "Location ID: " + locationID + ", Location Name: " + locationName );
+        Locations.Add( new Location( this, locationID, locationName ) );
+      }
+    }
+  }
+
+  public class Location
+  {
+    public String ID;
+    public String Name;
+    public Account Account;
+    public List<Zone> Zones = new List<Zone>();
+
+    public Location( Account account, String id, String name )
+    {
+      ID = id;
+      Name = name;
+      Account = account;
+    }
+
+    public void GetZones()
+    {
+      //GET Zones page
+      System.Net.HttpWebRequest request;
+      System.Net.HttpWebResponse response;
+      StreamReader streamReader;
+      String responseString;
+
+      request = (HttpWebRequest)WebRequest.Create( Account.ZonesURL + ID + "/Zones" );
+      request.KeepAlive = true;
+      request.CookieContainer = Account.Login();
+      request.Method = "GET";
+      request.ContentType = "application/x-www-form-urlencoded";
+      request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
+
+      response = (HttpWebResponse)request.GetResponse();
+      streamReader = new StreamReader( response.GetResponseStream() );
+      responseString = streamReader.ReadToEnd();
+      response.Close();
+
+      // pull out all "data-id=" values
+      int index = 1;
+      string thermostatID = null;
+      string thermostatName = null;
+      while( true )
+      {
+        thermostatID = Tools.Parse( responseString, "data-id=\"", "\" data-url", ref index );
+        if( thermostatID == null )
+          break; // TODO: might not be correct. Was : Exit Do
+        thermostatName = Tools.Parse( responseString, "<div class=\"location-name\">", "</div>", ref index ).Trim();
+        Tools.Log( "Thermostat ID: " + thermostatID + ", Thermostat Name: " + thermostatName );
+        Zones.Add( new Zone( Account, thermostatID, thermostatName ) );
+      }
+    }
+  }
+  
+  public class Zone
+  {
+    public Account Account;
+    public String ID;
+    public String Name;
+
+    public enum SystemSwitchEnum { Off = 2, Heat = 1, Cool = 3}
+    public enum ScheduleEnum { FollowSchedule = 0, HoldUntil = 1, PermanentHold = 2 }
+
+    public int CoolSetPoint;
+    public int HeatSetPoint;
+    public String Temperature;
+    public SystemSwitchEnum SystemSwitch;
+    public ScheduleEnum Schedule;
+    public int NextPeriod;
+
+    public Zone( Account account, String id, String name )
+    {
+      Account = account;
+      ID = id;
+      Name = name;
+    }
+
+    public void Update()
+    {
+      //GET Control page
+      System.Net.HttpWebRequest request;
+      System.Net.HttpWebResponse response;
+      StreamReader streamReader;
+      String responseString;
+
+      request = (HttpWebRequest)System.Net.HttpWebRequest.Create( Account.DeviceControlURL + ID );
+      request.KeepAlive = true;
+      request.CookieContainer = Account.Login();
+      request.Method = "GET";
+      request.ContentType = "application/x-www-form-urlencoded";
+      request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
+
+      response = (HttpWebResponse)request.GetResponse();
+      streamReader = new StreamReader( response.GetResponseStream() );
+      responseString = streamReader.ReadToEnd();
+      response.Close();
+
+      Tools.Log( responseString );
+
+      // pull out data  
+      CoolSetPoint = (int)double.Parse( Tools.Parse( responseString, "coolSetpoint, ", ");" ) );
+      //Tools.Log( "coolSetPoint=" + CoolSetPoint );
+      HeatSetPoint = (int)double.Parse( Tools.Parse( responseString, "heatSetpoint, ", ");" ) );
+      //Tools.Log( "heatSetPoint=" + HeatSetPoint );
+      Temperature = Tools.Parse( responseString, "dispTemperature, ", ");" );
+      //Tools.Log( "temperature=" + Temperature );
+
+      SystemSwitch = (SystemSwitchEnum)int.Parse( Tools.Parse( responseString, "systemSwitchPosition, ", ");" ) );
+      //Tools.Log( "SystemSwitch=" + SystemSwitch.ToString() );
+
+      Schedule = (ScheduleEnum)int.Parse( Tools.Parse( responseString, "statusHeat, ", ");" ) );
+      //Tools.Log( "Schedule=" + Schedule.ToString() );
+
+      if( SystemSwitch == SystemSwitchEnum.Heat )
+      {
+        NextPeriod = int.Parse( Tools.Parse( responseString, "heatNextPeriod, ", ");" ) );
+        //Tools.Log( "NextPeriod=" + NextPeriod );
+      }
+      if( SystemSwitch == SystemSwitchEnum.Cool )
+      {
+        NextPeriod = int.Parse( Tools.Parse( responseString, "coolNextPeriod, ", ");" ) );
+        //Tools.Log( "NextPeriod=" + NextPeriod );
+      }
+
+    }
+
+
+
+/* 
+-- off -> set cool permanent hold heat at 50
+CoolNextPeriod: null
+CoolSetpoint: null
+DeviceID: 416280
+FanMode: null
+HeatNextPeriod: null
+HeatSetpoint: 51
+StatusCool: 2
+StatusHeat: 2
+SystemSwitch: 1
+
+--> off
+CoolNextPeriod: null
+CoolSetpoint: null
+DeviceID: 416280
+FanMode: null
+HeatNextPeriod: null
+HeatSetpoint: null
+StatusCool: null
+StatusHeat: null
+SystemSwitch: 2
+ 
+--> heat hold until 8:15 pm
+CoolNextPeriod: 81
+CoolSetpoint: null
+DeviceID: 416280
+FanMode: null
+HeatNextPeriod: 81
+HeatSetpoint: 52
+StatusCool: 1
+StatusHeat: 1
+SystemSwitch: 1
+  
+--> follow schedule
+CoolNextPeriod: null
+CoolSetpoint: 99
+DeviceID: 416280
+FanMode: null
+HeatNextPeriod: null
+HeatSetpoint: 50
+StatusCool: 0
+StatusHeat: 0
+SystemSwitch: null
+*/ 
+
+    /// <summary>
+    /// Control the thermostat
+    /// </summary>
+    /// <param name="systemSwitch">Off, Heat, Cool</param>
+    /// <param name="schedule">FollowSchedule, HoldUntil, PermanentHold</param>
+    /// <param name="setPoint">Set poitn for heating or cooling</param>
+    /// <param name="holdHour">Hours to hold for. E.g. 2 holds for 2 hours.</param>
+    /// <param name="holdMinute">Minutes to hold for in multiples of 15 minutes. E.g. 2 = 30 minutes</param>
+    public void Set( SystemSwitchEnum systemSwitch, ScheduleEnum schedule, int setPoint, int nextPeriod )
+    {
+      System.Net.HttpWebRequest request;
+      System.Net.HttpWebResponse response;
+      StreamReader streamReader;
+      String responseString;
+
+      request = (HttpWebRequest)System.Net.HttpWebRequest.Create( "https://mytotalconnectcomfort.com/portal/Device/SubmitControlScreenChanges" );
+      request.KeepAlive = false;
+      request.ProtocolVersion = HttpVersion.Version10;
+      request.CookieContainer = Account.Login();
+      request.Method = "POST";
+      request.Accept = "application/json, text/javascript, */*";
+      request.ContentType = "application/json; charset=UTF-8";
+      request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
+      string postData = "{ \"DeviceID\":" + ID;
+      postData += ", \"CoolNextPeriod\":" + ((schedule == ScheduleEnum.HoldUntil) ? nextPeriod.ToString() : "null");
+      postData += ", \"CoolSetPoint\":" + ((systemSwitch == SystemSwitchEnum.Cool) ? setPoint.ToString() : "null");
+      postData += ", \"FanMode\":null";
+      postData += ", \"HeatNextPeriod\":" + ((schedule == ScheduleEnum.HoldUntil) ? nextPeriod.ToString() : "null");
+      postData += ", \"HeatSetPoint\":" + ((systemSwitch == SystemSwitchEnum.Heat) ? setPoint.ToString() : "null");
+      postData += ", \"StatusCool\":" + (int)schedule;
+      postData += ", \"StatusHeat\":" + (int)schedule;
+      postData += ", \"SystemSwitch\":" + (int)systemSwitch;
+      postData += "}";
+      byte[] byteArray = System.Text.Encoding.UTF8.GetBytes( postData );
+      request.ContentLength = byteArray.Length;
+      System.IO.Stream dataStream = request.GetRequestStream();
+      dataStream.Write( byteArray, 0, byteArray.Length );
+      dataStream.Close();
+
+      response = (HttpWebResponse)request.GetResponse();
+      streamReader = new StreamReader( response.GetResponseStream() );
+      responseString = streamReader.ReadToEnd();
+      response.Close();
+    }
+
+    /// <summary>
+    /// Control the thermostat
+    /// </summary>
+    /// <param name="systemSwitch">Off, Heat, Cool</param>
+    /// <param name="setPoint">Set poitn for heating or cooling</param>
+    /// <param name="holdHour">Hours to hold for. E.g. 2 holds for 2 hours.</param>
+    /// <param name="holdMinute">Minutes to hold for in multiples of 15 minutes. E.g. 2 = 30 minutes</param>
+    public void SetHoldUntil( SystemSwitchEnum systemSwitch, int setPoint, int holdHour, int holdMinute )
+    {
+      int nextPeriod = (DateTime.Now.Hour + holdHour) * 4 + (int)Math.Ceiling(DateTime.Now.Minute / 15.0) + holdMinute;
+      Set( systemSwitch, ScheduleEnum.HoldUntil, setPoint, nextPeriod );
+    }
+  }
+
+  public class Tools
+  {
+    public static String Parse( string s, string front, string back )
+    {
+      int index = 0;
+      return Parse( s, front, back, ref index );
+    }
+
+    public static String Parse( string s, string front, string back, ref int index )
+    {
+      int length = front.Length;
+      int startpos = s.IndexOf( front, index );
+      if( startpos == -1 )
+        return null;
+      int endpos = s.IndexOf( back, startpos + length );
+      if( endpos == -1 )
+        return null;
+      index = endpos + back.Length;
+      return s.Substring( startpos + length, endpos - (startpos + length) );
+    }
+
+    public static String XMLParse( string xml, string field )
+    {
+      String startpt = "<" + field + ">"; ;
+      String endpt = "</" + field + ">";
+      int length = startpt.Length;
+      int length2 = endpt.Length;
+      int startpos = xml.IndexOf( startpt );
+      int position = xml.IndexOf( startpt, startpos );
+      int endpos = xml.IndexOf( endpt );
+      return xml.Substring( position + length, endpos - (position + length) );
+    }
+
+    static internal void Log( String log )
+    {
+      System.Console.Out.WriteLine( log );
+    }
+  }
+}
---
+++ JSStat/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.ComponentModel;
+using System.Xml;
+
+namespace JSStat
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+
+    bool debugRun = false;
+    bool alwaysPulse = false;
+    int pulseTemp = 72;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private readonly BackgroundWorker worker = new BackgroundWorker();
+
+    private void Window_Loaded( object sender, RoutedEventArgs e )
+    {
+      worker.DoWork += pulseHeat;
+      worker.RunWorkerAsync();
+    }
+
+    private void pulseHeat( object sender, DoWorkEventArgs e )
+    {
+      log( "Loading thermostats." );
+
+      Account account = new Account( "**username**", "**password**" );
+      Location location = new Location( account, "**defualt location id**", "**defualt location**" );
+      location.GetZones();
+
+      while( true )
+      {
+        // if it's not cold months, go to sleep for another day.
+        if( DateTime.Now.Month > 3 && DateTime.Now.Month < 10 ) 
+        {
+          log( "It's not November, December, January, February, or March. Not pulsing heat. Sleeping for 1 day." );
+          Thread.Sleep( 1000 * 60 * 60 * 24 );
+          continue;
+        }
+
+        // look up outside temperature and windchill
+        try
+        {
+          // get the tempearture and windchill
+          WebClient client = new WebClient();
+          string reply = client.DownloadString( "http://w1.weather.gov/xml/current_obs/KBED.xml" );
+          XmlDocument doc = new XmlDocument();
+          doc.LoadXml( reply );
+          float outsideTemperature = float.Parse( doc.GetElementsByTagName( "temp_f" )[0].InnerText );
+          float outsideWindchill = outsideTemperature;
+          try
+          {
+            outsideWindchill = float.Parse( doc.GetElementsByTagName( "windchill_f" )[0].InnerText );
+          }
+          catch( Exception ) { }
+
+          if( !alwaysPulse && !debugRun && outsideTemperature > 29.0 && (outsideTemperature > 31 || outsideWindchill > 25.0) )
+          {
+            log( "Nothing to do. Outside Temperature = " + outsideTemperature + " Wind Chill = " + outsideWindchill );
+            log( "Sleeping for 30 minutes." );
+            Thread.Sleep( (debugRun ? 10 : 1000) * 60 * 30 );
+            continue;
+          }
+          log( "Brrrr... it's cold. Outside Temperature = " + outsideTemperature + " Wind Chill = " + outsideWindchill );
+        }
+        catch( Exception e2 )
+        {
+          log( "Error looking up temperature, pulsing heat just in case.", LogType.Important );
+          log( e2 );
+        }
+
+        // Pulse the heat every hour when it's cold outside.
+        System.DateTime sleepUntil = DateTime.Now.AddMinutes( debugRun ? 1 : 60 );
+
+        // if we got here, it's cold outside. Loop through zones pulsing temperature.
+        foreach( Zone zone in location.Zones )
+        {
+          try
+          {
+            log( "Found thermostat = " + zone.Name + ". Polling data." );
+            zone.Update();
+            log( "Thermostat current set point is " + zone.HeatSetPoint + " mode is " + zone.Schedule.ToString() );
+
+            if( zone.HeatSetPoint > 65 )
+            {
+              log( "Found thermostat = " + zone.Name + " current set point is " + zone.HeatSetPoint + ". No need to pulse heat when set point is > 65." );
+              continue;
+            }
+
+            log( "Pulsing heat for thermostat " + zone.Name, LogType.Important );
+
+            // save the zone values
+            Zone.ScheduleEnum schedule = zone.Schedule;
+            int heatSetpoint = zone.HeatSetPoint;
+            int nextPeriod = zone.NextPeriod;
+
+            zone.SetHoldUntil( Zone.SystemSwitchEnum.Heat, pulseTemp, 0, 1 );
+            log( "Sleeping for 7 minutes." );
+            Thread.Sleep( (debugRun ? 10 : 1000) * 60 * 7 );
+            log( "Returning zone to previous settings." );
+            zone.Set( Zone.SystemSwitchEnum.Heat, schedule, heatSetpoint, nextPeriod );
+          }
+          catch( Exception e3 )
+          {
+            log( e3 );
+          }
+
+        }
+
+        log( "Sleeping unitl next hour is up." );
+        Thread.Sleep( (sleepUntil - DateTime.Now) );
+      }
+    }
+
+    FlowDocument logFlowDocument;
+    int lineCount = 0;
+    enum LogType { Normal, Important, Error };
+
+    private void log( String message )
+    {
+      log( message, LogType.Normal );
+    }
+
+    private void log( Exception e )
+    {
+      log( e.ToString(), LogType.Error );
+      log( e.StackTrace.ToString(), LogType.Error );
+    }
+
+    private void log( String message, LogType logType )
+    {
+      if( !richTextBox.Dispatcher.CheckAccess() )
+      {
+        richTextBox.Dispatcher.Invoke(
+          System.Windows.Threading.DispatcherPriority.Normal,
+          new Action(
+            delegate() { log( message, logType ); }
+          ) 
+        );
+        return;
+      }
+
+      // clear the log every 1,000 entries.
+      lineCount++;
+      if( lineCount > 1000 )
+      {
+        lineCount = 0;
+        logFlowDocument = null;
+      }
+
+      if( logFlowDocument == null )
+      {
+        logFlowDocument = new FlowDocument();
+        Style style = new Style( typeof( Paragraph ) );
+        style.Setters.Add( new Setter( Block.MarginProperty, new Thickness( 0 ) ) );
+        logFlowDocument.Resources.Add( typeof( Paragraph ), style );
+        richTextBox.Document = logFlowDocument;
+      }
+
+      message = DateTime.Now.ToShortTimeString() + " " + message;
+      Paragraph p;
+      if( logType == LogType.Important ) 
+        p = new Paragraph( new Bold( new Run( message ) ) );
+      else
+        p = new Paragraph( new Run( message ) );
+      if( logType == LogType.Error )
+      p.Foreground = Brushes.Red;
+      logFlowDocument.Blocks.Add( p );
+      richTextBox.ScrollToEnd();
+     
+    }
+
+  }
+}
